18fb74656f67419c2d834c48290a833ecdaaf187
Dinatolemy <dina.a.tolemy@gmail.com>
2019-11-19 11:48:22 +1100

    moving the js code to the js file and editing the functions

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,15 +1,63 @@
 
 
-   function generatePassword() {
+function generatePassword() {
 
-            var wantNumbers = confirm("do you need numbers in your password");
+    var wantNumbers = confirm("do you need numbers in your password");
 
-            if (wantNumbers) {
+    if (wantNumbers) {
 
 
-                document.getElementById("password").innerHTML = Math.floor(Math.random() * 20);
-                
+        function makePass(length) {
+            var charResult = '';
+            var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+            var charactersLength = characters.length;
+
+            for (var i = 0; i < length; i++) {
+                charResult += characters.charAt(Math.floor(Math.random() * charactersLength));
             }
-            else{}
+
+
+            return charResult;
 
         }
+        function makePass2(length) {
+            var numResult = '';
+            var numbers = '0123456789';
+            var numbersLength = numbers.length;
+            for (var i = 0; i < length; i++) {
+                numResult += numbers.charAt(Math.floor(Math.random() * numbersLength));
+            }
+            return numResult;
+
+        }
+
+
+       document.getElementById("password").innerHTML =  makePass2(2)+makePass(6);
+       // document.getElementById("password").innerHTML = makePass2(2);
+
+
+
+    }
+    else {
+
+        function makePass(length) {
+            var result = '';
+            var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+            var charactersLength = characters.length;
+            for (var i = 0; i < length; i++) {
+                result += characters.charAt(Math.floor(Math.random() * charactersLength));
+            }
+            return result;
+
+        }
+
+        document.getElementById("password").innerHTML = makePass(6);
+
+
+
+    }
+
+
+
+
+}
